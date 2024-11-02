a0cab03606dd7d90474dc14ea5b77139fadffc90
leafyin <chinayinye@icloud.com>
2024-11-03 00:05:06 +0800

    添加TextMesh Pro,射线投射

--- Assets/Script/RubyController.cs
+++ Assets/Script/RubyController.cs
@@ -97,6 +97,24 @@ public class RubyController : MonoBehaviour
         {
             Launch();
         }
+
+        // 射线投射
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, 
+                lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            // 这里为什么是hit 为布尔值
+            if (hit != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (hit.collider != null)
+                {
+                    character.DisplayDialog();
+                    //Debug.Log("Raycast has hit the object " + hit.collider.gameObject);
+                }
+            }
+        }
+
     }
 
     public void ChangeHealth(int amount, bool type)
